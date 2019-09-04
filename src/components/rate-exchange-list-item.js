1d0817373281ce507f810f9283b0880f4baa2be3
BinaryDump <BinaryDump>
2019-09-04 19:16:57 +0200

    add chart

--- src/components/rate-exchange-list-item.js
+++ src/components/rate-exchange-list-item.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import ReactChartKick, {AreaChart} from 'react-chartkick'
+import Chart from 'chart.js'
 
+ReactChartKick.addAdapter(Chart);
 class RateExchangeListItem extends Component {
     render () {
-        const{flag,name,rates} = this.props.rateExchange;
+        const{flag,name,rates, currencyCode} = this.props.rateExchange;
         return (
             <tr>
                 <td>{name}<br/><img src={flag} height="60" width="100" /></td>
-                <td>Graphique</td>
+                <td className="col-md-12"><AreaChart data={formatData(rates,currencyCode)} xtitle="Date" ytitle={currencyCode} /></td>
             </tr>
         )
     }
+}
+
+function formatData(rates,currencyCode){
+    // debugger;
+   return Object.keys(rates).map(date => {
+        return [ date , rates[date][currencyCode] ]
+    })
 }
 
 export default RateExchangeListItem
